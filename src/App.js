9c0f9da5b0917735da99bc29b42262f2f0a89d25
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-05-11 19:15:23 +0530

    useContext and useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './App.css';
-import DataFetching from './components/DataFetching';
+import ComponentA from './component/ComponentA';
+import ComponentB from './component/ComponentB';
+import ComponentC from './component/ComponentC';
+
+const initialState = 0
+const reducer = (state, action) => {
+	switch(action) {
+		case 'increment':
+			return state + 1
+		case 'decrement':
+			return state - 1
+		case 'reset':
+			return initialState
+		default:
+			return state
+	}
+}
+
+// export const UserContext = React.createContext()
+// export const ChannelContext = React.createContext()
+export const CountContext = React.createContext()
 
 function App() {
+	const [count, dispatch] = useReducer(reducer, initialState)
 	return (
-		<div className="App">
-			<DataFetching />
-		</div>
+		<CountContext.Provider 
+			value={{
+				countState: count,
+				countDispatch: dispatch
+			}}
+		>
+			<div className="App">
+				Count - {count}
+				<ComponentA />
+				<ComponentB />
+				<ComponentC />
+			</div>
+		</CountContext.Provider>
 	);
 }
 
 export default App;
+
+// {/* <UserContext.Provider value={'Rick'}>
+// 				<ChannelContext.Provider value={'Lykos'}>
+// 					<ComponentC />
+// 				</ChannelContext.Provider>
+// 			</UserContext.Provider> */}

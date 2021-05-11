f2b1480079754fb1f6c820b857ba69610efe4dbc
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-05-11 22:46:41 +0530

    Initial State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import './App.css';
-import CounterOne from './component/CounterOne';
-import CounterTwo from './component/CounterTwo';
-import DocTitleOne from './component/DocTitleOne';
-import DocTitleTwo from './component/DocTitleTwo';
-import UserForm from './component/UserForm';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <DocTitleOne />
-			<DocTitleTwo /> */}
-			{/* <CounterOne />
-			<CounterTwo /> */}
-			<UserForm />
+			Initial App
 		</div>
 	);
 }

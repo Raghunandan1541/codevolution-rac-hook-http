0f78aa210b9c2c5002ea7a9b2ee61b2f01f40d15
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-05-11 20:43:29 +0530

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import './App.css';
-import DataFetchingOne from './components/DataFetchingOne';
-import DataFetchingTwo from './components/DataFetchingTwo';
-
-// export const UserContext = React.createContext()
-// export const ChannelContext = React.createContext()
+import ParentComponent from './component/ParentComponent';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <DataFetchingOne /> */}
-			<DataFetchingTwo />
+			<ParentComponent />
 		</div>
 	);
 }
 
 export default App;
-
-// {/* <UserContext.Provider value={'Rick'}>
-// 				<ChannelContext.Provider value={'Lykos'}>
-// 					<ComponentC />
-// 				</ChannelContext.Provider>
-// 			</UserContext.Provider> */}

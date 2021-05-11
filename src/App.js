cec308160c4b8cf161ddebc3305d2f017f77799a
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-05-11 22:44:45 +0530

    Custom Hook 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './App.css';
+import CounterOne from './component/CounterOne';
+import CounterTwo from './component/CounterTwo';
 import DocTitleOne from './component/DocTitleOne';
 import DocTitleTwo from './component/DocTitleTwo';
+import UserForm from './component/UserForm';
 
 function App() {
 	return (
 		<div className="App">
-			<DocTitleOne />
-			<DocTitleTwo />
+			{/* <DocTitleOne />
+			<DocTitleTwo /> */}
+			{/* <CounterOne />
+			<CounterTwo /> */}
+			<UserForm />
 		</div>
 	);
 }
